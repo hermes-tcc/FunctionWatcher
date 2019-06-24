1c3c04993c59f1191a0093e5490c2342b0d22ff6
tiagonapoli <napoli.tiago96@gmail.com>
2019-06-24 13:25:27 -0300

    Add better error handling. Remove unnused middleware. Add new route

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,45 @@
-import { executeRoute } from './routes/execute'
-import { RedisEvents } from './resources/RedisEvents'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import morgan from 'morgan'
+import { RedisEvents } from './resources/RedisEvents'
+import { runRoute } from './routes/run/index'
+import { Logger } from './utils/Logger'
+import { prepareHandler } from './utils/runner'
 
-try {
-  const server = express()
+const server = express()
+server.use(morgan('dev'))
+server.use('/run', runRoute)
 
-  server.use(express.json())
-  server.use(morgan('dev'))
+server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  if (!err.getStatusCode) err.getStatusCode = () => 500
+  if (!err.getResponseObject) {
+    err.getResponseObject = () => {
+      return { error: 'InternalServerError', message: 'Something broke in the function server' }
+    }
+  }
+  Logger.error(`Error ${err.constructor.name}`, err)
+  if (err.detail) Logger.info(`Error details`, err.detail)
+  res.setHeader('Connection', 'close')
+  res.status(err.getStatusCode()).send(err.getResponseObject())
+})
 
-  server.use('/execute', executeRoute)
-  server.use('/', (_, res) => {
-    res.status(404).send('Not found')
-  })
+server.use('/', (_, res) => {
+  res.status(404).send('Not found')
+})
 
-  const PORT = process.env.PORT || 8080
-  server.listen(PORT, () => {
-    console.log(`Server listening on port http://localhost:${PORT}`)
-    RedisEvents.okEvent()
-  })
-} catch (e) {
-  console.log(e)
-  RedisEvents.errorEvent()
+const initServer = async () => {
+  try {
+    await prepareHandler()
+
+    const PORT = process.env.PORT || 8080
+
+    server.listen(PORT, () => {
+      Logger.info(`Server listening on port http://localhost:${PORT}`)
+      RedisEvents.startupSuccess()
+    })
+  } catch (err) {
+    Logger.error('Error on server init', err)
+    RedisEvents.startupError()
+  }
 }
+
+initServer()
