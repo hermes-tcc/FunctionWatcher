aaa060d151192cbc5849b08b6a91aec2c4d6db2b
tiagonapoli <napoli.tiago96@gmail.com>
2019-06-24 13:24:16 -0300

    Major improve Subprocess implementation

--- src/resources/Subprocess.ts
+++ src/resources/Subprocess.ts
@@ -1,81 +1,125 @@
-import { spawn } from "child_process";
-import { Stream, Readable } from "stream";
-import Path from "path";
+import { ChildProcess, spawn } from 'child_process'
+import { Readable, Writable } from 'stream'
+import { MaxOutputSizeReached, NonZeroReturnCode } from '../errors/RunnerErrors'
+import { QueueBuffer } from '../utils/CircularBuffer'
+import { Waiter } from '../utils/CustomPromises'
+import { Logger } from '../utils/Logger'
 
-class StringStream extends Stream.Readable {
-  str: string;
+const KBYTE = 1024
+const MBYTE = 1024 * 1024
+const MAX_QUEUE_BUFFER_SIZE = 10 * KBYTE
+const MAX_OUTPUT_SIZE = 10 * MBYTE
 
-  constructor(str: string, options?: any) {
-    super(options);
-    this.str = str;
-  }
-
-  _read() {
-    this.push(this.str);
-    this.push(null);
-  }
+interface SubprocessConstructor {
+  path: string
+  id: string
+  args?: string[]
 }
 
-class Subprocess {
-  filePath: string;
-  process: any;
-  resolveReturnCode: any;
-  returnCode: Promise<number>;
-  exited: boolean;
-  output: string;
-  errOutput: string;
-  inputObj: any;
-
-  constructor(pathArr: string[], inputObj: any) {
-    this.filePath = Path.join(...pathArr);
-    this.inputObj = inputObj;
-    this.returnCode = new Promise(resolve => {
-      this.resolveReturnCode = resolve;
-    });
-
-    this.output = "";
-    this.errOutput = "";
-  }
-
-  getInputStream(): Readable {
-    if (this.inputObj.type == "string")
-      return new StringStream(this.inputObj.input);
-
-    throw new Error(
-      `Input type not supported, undefined or invalid: ${this.inputObj.type}`
-    );
-  }
-
-  start() {
-    this.process = spawn(this.filePath);
-    console.log("Proess", this.filePath);
-
-    this.process.on("close", (ret: number) => {
-      this.resolveReturnCode(ret);
-      this.exited = true;
-    });
-    this.process.on("error", (err: any) => console.log("Error catch", err));
-    this.process.stdout.on("data", (data: string) => (this.output += data));
-    this.process.stderr.on("data", (data: string) => (this.errOutput += data));
-
-    this.getInputStream()
-      .pipe(this.process.stdin)
-      .on("error", (e: any) => {
-        console.log("PIPE ERROR CAPTURED", e);
-      });
-  }
-
-  getOutput = async () => {
-    await this.returnCode;
-    return this.output;
-  };
-
-  getErrOutput = async () => {
-    await this.returnCode;
-    return this.errOutput;
-  };
-
-  kill() {}
+interface SubprocessIO {
+  in?: Readable
+  err?: Writable
+  out?: Writable
 }
 
-export { Subprocess, StringStream };
+export class Subprocess {
+  private path: string
+  private args: string[]
+  private process: ChildProcess
+  private id: string
+
+  private returnCode: number
+  private returnSig: string
+
+  private out: QueueBuffer
+  private err: QueueBuffer
+  private outputSize: number
+  private runError?: Error
+
+  private doneProcess: Waiter<any>
+
+  constructor({ path, args, id }: SubprocessConstructor) {
+    this.id = id
+    this.path = path
+    this.args = args ? args : []
+    this.doneProcess = new Waiter()
+  }
+
+  public start(io: SubprocessIO) {
+    Logger.info(`[Subprocess] Spawn process: ${this.id}`, { path: this.path, args: this.args })
+    try {
+      this.process = spawn(this.path, this.args)
+
+      this.process.on('close', (ret: number, signal: string) => {
+        Logger.info(`[Subprocess] Process closed: ${this.id}`, { ret, signal })
+        if (ret !== 0 && !this.runError) this.runError = new NonZeroReturnCode(ret)
+        this.doneProcess.resolve()
+        this.returnCode = ret
+        this.returnSig = signal
+      })
+
+      this.err = this.setupOutputBuffer(io.err, this.process.stderr)
+      this.out = this.setupOutputBuffer(io.out, this.process.stdout)
+
+      this.process.on('error', (err: any) =>
+        Logger.error(`[Subprocess] Error event catch ${this.id}`, err)
+      )
+
+      if (io.in) {
+        io.in
+          .pipe(this.process.stdin)
+          .on('error', (e: any) => Logger.error(`[Subprocess] Pipe error ${this.id}`, e))
+      }
+    } catch (err) {
+      Logger.error(`[Subprocess] Error catch ${this.id}`, err)
+      this.doneProcess.reject(err)
+    }
+  }
+
+  public getErr() {
+    return this.err.getString()
+  }
+
+  public getOut() {
+    return this.out.getString()
+  }
+
+  public getExitCode() {
+    return this.returnCode
+  }
+
+  public getExitSignal() {
+    return this.returnSig
+  }
+
+  public finish() {
+    return this.doneProcess.finish()
+  }
+
+  public getError() {
+    return this.runError
+  }
+
+  public kill() {
+    this.process.kill()
+  }
+
+  private setupOutputBuffer = (outputStream: Writable, stdStream: Readable) => {
+    this.outputSize = 0
+    const queueBuffer = new QueueBuffer(MAX_QUEUE_BUFFER_SIZE)
+    if (outputStream) stdStream.pipe(outputStream)
+    stdStream.on('data', (data: Buffer) => {
+      const str = data.toString('utf-8')
+      this.outputSize += str.length
+      if (this.outputSize > MAX_OUTPUT_SIZE && this.runError == null) {
+        this.runError = new MaxOutputSizeReached(MAX_OUTPUT_SIZE)
+        this.process.emit('error', this.runError)
+        if (outputStream) stdStream.unpipe(outputStream)
+        this.kill()
+      }
+      queueBuffer.push(str)
+    })
+
+    return queueBuffer
+  }
+}
