0421b81a1f49bc1283e1e4d122c64c937a24ace6
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-02 21:24:16 -0300

    Bug fix on then function

--- src/utils/CustomPromises.ts
+++ src/utils/CustomPromises.ts
@@ -54,7 +54,7 @@ export class Waiter<T> {
     return this.done
   }
 
-  public then = (onFulfilled: (...args: any) => any, onRejected?: (...args: any) => any) => {
+  public then = (onFulfilled: (...args: any) => any, onRejected: (...args: any) => any) => {
     this.promise.then(onFulfilled, onRejected)
   }
 }
