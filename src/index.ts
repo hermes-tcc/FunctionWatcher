0b76a105c7042a07de8c2ec01f9ecd9cb383464e
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-05 11:13:45 -0300

    Add exit logs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import { Logger } from './utils/Logger'
 import { WatcherServer } from './WatcherServer'
+
+process.on('exit', code => {
+  Logger.info(`About to exit with code: ${code}`)
+})
 
 WatcherServer.start()
