e190d209423ac1241239393455081feb8ae98474
tiagonapoli <napoli.tiago96@gmail.com>
2019-07-30 22:54:14 -0300

    Remove unused limit

--- src/limits/index.ts
+++ src/limits/index.ts
@@ -4,7 +4,3 @@ const MBYTE = 1024 * KBYTE
 export const MAX_INPUT_SIZE = 100 * MBYTE
 export const MAX_OUTPUT_SIZE = 10 * MBYTE
 export const MAX_QUEUE_BUFFER_SIZE = 10 * KBYTE
-
-const SECOND = 1000
-const MINUTE = 60 * SECOND
-export const MAX_BUSBOY_FINISH_TIME = 1 * MINUTE
